84e800c8f25a3190f1d4d4b0c766b5325fd82a8d
RafaelNercessian <rafanercessian@gmail.com>
2017-06-30 13:46:33 -0300

    analisando log pelo IP

---
+++ cap2-parte1-analisando-log-por-IP.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $1 =~ [0-9]?{2}[1-9].[0-9]?{2}[0-9].[0-9]?{2}[0-9].[0-9][0-9]?{2} ]]
+then
+	cat ~/LOG/LOG-TOMCAT/* | grep "$1"
+	if [ $? -ne 0 ]
+  	then
+		echo "O endereço IP não existe no arquivo"
+	fi
+else
+	echo "O argumento passado não é  um endereço IP válido"
+fi
+	
