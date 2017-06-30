7f20e63ddc5287e17f0a2be231b11a3190686d13
RafaelNercessian <rafanercessian@gmail.com>
2017-06-30 12:43:50 -0300

    Código corrigido cap1.2

--- cap1.2-processos-consomem-mais-memoria.sh
+++ cap1.2-processos-consomem-mais-memoria.sh
@@ -2,6 +2,7 @@
 processos_consomem_mais_memoria(){
 	if [ ! -d LOG ]
 	then
+		cd ~
 		mkdir LOG
 	fi
 	for pid in $(ps -eo pid --sort -size | head -n11 | grep '[0-9]')
@@ -11,7 +12,8 @@ processos_consomem_mais_memoria(){
 		echo -n "$(date +%Y%m%d,%H:%M:%S,)" >> $nome_do_processo.log
 		tamanho=$(ps -o size --pid $pid | grep '[0-9]')
 		echo "$(bc<<<"scale=2;$tamanho/1024") MB " >> $nome_do_processo.log
-		cp $nome_do_processo.log LOG/$nome_do_processo.log
+		cp $nome_do_processo.log ~/LOG/$nome_do_processo.log
+		rm *.log
 	done
 }
 
