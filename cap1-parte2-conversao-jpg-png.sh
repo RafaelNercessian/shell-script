e81c595e3add4b86bee124f97bfec805ed6bf286
RafaelNercessian <rafanercessian@gmail.com>
2017-06-30 10:05:07 -0300

    Cap1-parte2-e-parte3

---
+++ cap1-parte2-conversao-jpg-png.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+convert ~/Downloads/IMAGENS-LIVROS/$1.jpg ~/Downloads/IMAGENS-LIVROS/$1.png
---
+++ cap1-parte3-conversao-jpg-png.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CAMINHO=~/Downloads/IMAGENS-LIVROS
+convert $CAMINHO/$1.jpg $CAMINHO/$1.png
+convert $CAMINHO/$2.jpg $CAMINHO/$2.png
