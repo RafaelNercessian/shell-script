3da2f1ac31a0c215b1a7828603a68bd69c3c7de6
RafaelNercessian <rafanercessian@gmail.com>
2017-07-01 14:47:34 -0300

    Enviando email memoria em nivel critico

--- cap3-monitora-apache-envia-email.sh
+++ cap3-monitora-apache-envia-email.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 email_de_contato="rafanercessian@gmail.com"
 response=$(curl --write-out %{http_code} --connect-timeout 5 --silent --output /dev/null http://localhost)
@@ -9,6 +9,10 @@ else
 mail -s "O servidor parou" $email_de_contato << EOF
 O servidor apresentou um problema e parou.
 EOF
+if [ $? -eq 0 ]
+then
+	echo "O email foi enviado"
+fi
 	service apache2 restart
 	if [ $? -eq 0 ]
 	then
---
+++ cap4-envia-email-memoria-nivel-critico.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+memoria_atual=$(free | grep -i "mem" | awk '{ print $3 }')
+memoria_total=$(free | grep -i "mem" | awk '{ print $2 }')
+relacao_atual_total=$(echo $memoria_atual/$memoria_total *100 | bc -l | awk -F. '{ print $1 }')
+LIMIAR=90
+if [ $relacao_atual_total -gt $LIMIAR ]
+then
+	echo "O consumo de memória está acima do limite. Enviando email para o administrador"
+mail -s 'Alerta servidor' rafanercessian@gmail.com <<EOF
+Consumo de memória em nível crítico, uso de $(free -h | grep -i "mem" | awk '{ print $3 }')
+EOF
+else
+	echo "O consumo de memória está dentro do limite"
+fi
