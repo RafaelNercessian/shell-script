2ecab7a73718c952b4c1a9e8e931b78013345b09
RafaelNercessian <rafanercessian@gmail.com>
2017-07-03 15:38:55 -0300

    Alterando script para recuperar dados do servidor

--- cap5-recupera-dados-mysql.sh
+++ cap5-recupera-dados-mysql.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-
-aws s3 sync s3://curso-shell-script-backup-mysql/$(date +%F) ~/backups/Backup-S3-Amazon/$(date +%F)
+CAMINHO="/home/rafael/backups/Backup-S3-Amazon"
+aws s3 sync s3://curso-shell-script-backup-mysql/$(date +%F) $CAMINHO/$(date +%F)
 if [ $? -eq 0 ]
 then
-	cd ~/backups/Backup-S3-Amazon/$(date +%F)/casadocodigo
+	cd $CAMINHO/$(date +%F)/casadocodigo
 	 for arquivo in *
                 do
                         $(gunzip -c $arquivo| mysql -u root -D casadocodigo)
                 done
         echo "Dados recuperados com sucesso"
 else
-        aws s3 sync s3://curso-shell-script-backup-mysql/$(date -d '-1 day' '+%Y-%m-%d') ~/backups/Backup-S3-Amazon/$(date -d '-1 day' '+%Y-%m-%d')
+        aws s3 sync s3://curso-shell-script-backup-mysql/$(date -d '-1 day' '+%Y-%m-%d') $CAMINHO/$(date -d '-1 day' '+%Y-%m-%d')
         if [ $? -ne 0 ]
         then
                 echo "O backup não pode ser realizado"
         else
-		cd ~/backups/Backup-S3-Amazon/$(date -d '-1 day' '+%Y-%m-%d')/casadocodigo
+		cd $CAMINHO/$(date -d '-1 day' '+%Y-%m-%d')/casadocodigo
                 for arquivo in *
                         do
                                 $(gunzip -c $arquivo| mysql -u root -D casadocodigo)
