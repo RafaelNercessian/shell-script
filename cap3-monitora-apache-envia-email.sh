5d06a3c206ed2c1e38ca1d62bb2af2d4cba3b284
RafaelNercessian <rafanercessian@gmail.com>
2017-07-03 14:16:38 -0300

    alterando código monitora apache

--- cap3-monitora-apache-envia-email.sh
+++ cap3-monitora-apache-envia-email.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 email_de_contato="rafanercessian@gmail.com"
 response=$(curl --write-out %{http_code} --connect-timeout 5 --silent --output /dev/null http://localhost)
@@ -6,16 +6,8 @@ if [ $response -eq 200 ]
 then
 	echo "O servidor está funcionando"
 else
-mail -s "O servidor parou" $email_de_contato << EOF
+mail -s "O servidor parou" rafanercessian@gmail.com << EOF
 O servidor apresentou um problema e parou.
 EOF
-if [ $? -eq 0 ]
-then
-	echo "O email foi enviado"
-fi
-	service apache2 restart
-	if [ $? -eq 0 ]
-	then
-		echo "O servidor reiniciou"
-	fi
+nohup systemctl restart apache2
 fi
