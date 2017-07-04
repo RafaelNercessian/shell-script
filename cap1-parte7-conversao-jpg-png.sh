604d5efe58249ff6e05acde65646629fdfe9da41
RafaelNercessian <rafanercessian@gmail.com>
2017-07-04 15:42:37 -0300

    Recursão aparentemente funcionando

--- cap1-parte7-conversao-jpg-png.sh
+++ cap1-parte7-conversao-jpg-png.sh
@@ -8,6 +8,8 @@ recursao(){
 					if [ -z $arquivo_dentro_diretorio_sem_ext ]
 					then
 						cd ~/Downloads/IMAGENS-LIVROS/$1
+						arquivo_dentro_diretorio_sem_ext=$(ls $arquivo_dentro_diretorio | awk -F. '{ print $1 }')
+						convert $arquivo_dentro_diretorio_sem_ext.jpg $arquivo_dentro_diretorio_sem_ext.png
 					else
 						convert $arquivo_dentro_diretorio_sem_ext.jpg $arquivo_dentro_diretorio_sem_ext.png
 					fi		
