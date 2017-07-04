26c7614dc62b37edf670ef57860b8c2b425a308d
RafaelNercessian <rafanercessian@gmail.com>
2017-07-04 10:51:43 -0300

    Ajustando recursao

--- cap1-parte7-conversao-jpg-png.sh
+++ cap1-parte7-conversao-jpg-png.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 recursao(){
-	for arquivo in ~/Downloads/IMAGENS-LIVROS/*
+	cd ~/Downloads/IMAGENS-LIVROS/
+	for arquivo in *
 	do
 		if [ -d $arquivo ]
 		then
-			for arquivo_dentro_diretorio in $arquivo/*
+			cd $arquivo
+			if [ ! -d "png" ]
+			then
+				mkdir png
+			fi
+			for arquivo_dentro_diretorio in *.jpg
 			do
 				arquivo_dentro_diretorio_sem_ext=$(ls $arquivo_dentro_diretorio | awk -F. '{ print $1 }')
-				echo $arquivo_dentro_diretorio_sem_ext
-				convert $arquivo_dentro_diretorio_sem_ext.jpg $arquivo_dentro_diretorio_sem_ext.png 
+				convert $arquivo_dentro_diretorio_sem_ext.jpg png/$arquivo_dentro_diretorio_sem_ext.png
 			done
+		cd ~/Downloads/IMAGENS-LIVROS/
 		else
 			recursao
 		fi
