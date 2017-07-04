7d1fc701eab3477c2018454a5f86c88c12f04e5c
RafaelNercessian <rafanercessian@gmail.com>
2017-07-04 15:33:20 -0300

    Recursão com problemas

--- cap1-parte7-conversao-jpg-png.sh
+++ cap1-parte7-conversao-jpg-png.sh
@@ -1,26 +1,36 @@
-#!/bin/bash
-
 recursao(){
-	cd ~/Downloads/IMAGENS-LIVROS/
+
+			for arquivo_dentro_diretorio in *
+			do
+				if [ ! -d $arquivo_dentro_diretorio ]
+				then
+					arquivo_dentro_diretorio_sem_ext=$(ls $arquivo_dentro_diretorio | awk -F. '{ print $1 }')
+					if [ -z $arquivo_dentro_diretorio_sem_ext ]
+					then
+						cd ~/Downloads/IMAGENS-LIVROS/$1
+					else
+						convert $arquivo_dentro_diretorio_sem_ext.jpg $arquivo_dentro_diretorio_sem_ext.png
+					fi		
+				else
+				   cd ~/Downloads/IMAGENS-LIVROS/$1/$arquivo_dentro_diretorio
+				   recursao $arquivo_dentro_diretorio
+				fi
+			done
+}
+
+
+
+main(){ 
+	cd ~/Downloads/IMAGENS-LIVROS
 	for arquivo in *
 	do
 		if [ -d $arquivo ]
 		then
 			cd $arquivo
-			if [ ! -d "png" ]
-			then
-				mkdir png
-			fi
-			for arquivo_dentro_diretorio in *.jpg
-			do
-				arquivo_dentro_diretorio_sem_ext=$(ls $arquivo_dentro_diretorio | awk -F. '{ print $1 }')
-				convert $arquivo_dentro_diretorio_sem_ext.jpg png/$arquivo_dentro_diretorio_sem_ext.png
-			done
+			recursao $arquivo
 		cd ~/Downloads/IMAGENS-LIVROS/
-		else
-			recursao
 		fi
 	done
 }
 
-recursao
+main
