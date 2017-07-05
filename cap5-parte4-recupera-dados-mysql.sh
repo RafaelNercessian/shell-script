370e1001a6438effb7035e901bb401d4128bc6a5
RafaelNercessian <rafanercessian@gmail.com>
2017-07-05 11:00:29 -0300

    Alterando o código recupera dados

--- cap5-parte4-recupera-dados-mysql.sh
+++ cap5-parte4-recupera-dados-mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CAMINHO="/home/rafael/backups/Backup-S3-Amazon"
 aws s3 sync s3://curso-shell-script-backup-mysql/$(date +%F) $CAMINHO/$(date +%F)
-if [ $(ls - A $CAMINHO/$(date +%F)) -ne " " ]
+if [ ! -z $(ls $CAMINHO/$(date +%F)) ]
 then
 	cd $CAMINHO/$(date +%F)/casadocodigo
 	 for arquivo in *
@@ -10,18 +10,5 @@ then
                 done
         echo "Dados recuperados com sucesso"
 else
-	rm -r $CAMINHO/$(date +%F)
-        aws s3 sync s3://curso-shell-script-backup-mysql/$(date -d '-1 day' '+%Y-%m-%d') $CAMINHO/$(date -d '-1 day' '+%Y-%m-%d')
-        if [ $? -ne 0 ]
-        then
-                echo "O backup não pode ser realizado"
-        else
-		cd $CAMINHO/$(date -d '-1 day' '+%Y-%m-%d')/casadocodigo
-                for arquivo in *
-                        do
-                                $(gunzip -c $arquivo| mysql -u root -D casadocodigo)
-                        done
-                echo "Dados recuperados com sucesso"
-        fi
-
+        echo "Os dados não foram recuperados"
 fi
