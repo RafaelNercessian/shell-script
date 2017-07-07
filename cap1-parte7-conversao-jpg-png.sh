f292f05a99cca48ad5a728e71daf9074d442d7f1
RafaelNercessian <rafanercessian@gmail.com>
2017-07-07 11:20:14 -0300

    Update cap1-parte7-conversao-jpg-png.sh

--- cap1-parte7-conversao-jpg-png.sh
+++ cap1-parte7-conversao-jpg-png.sh
@@ -1,44 +1,44 @@
-recursao(){
-	cd $1
-	for conteudo_do_arquivo in *
-	do
-		if [ -d $conteudo_do_arquivo ]
-		then
-			recursao $(find ~/Downloads -name $conteudo_do_arquivo)
-		else
-			local imagem=$(find ~/Downloads -name $conteudo_do_arquivo)
-			local arquivo_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
-			convert $arquivo_sem_extensao.jpg $arquivo_sem_extensao.png
-		fi
-	done
-}		
+#!/bin/bash
 
+converte_imagem(){
 
-main(){ 
-	cd ~/Downloads/imagens-livros
-	for arquivo in *
-	do
-		if [ -d $arquivo ]
-		then
-			cd $arquivo
-			for dentro_do_arquivo in *	
-			do	
-				if [ -d $(find ~/Downloads -name $dentro_do_arquivo) ]
-				then	
-					recursao $(find ~/Downloads -name $dentro_do_arquivo)
-					
-				else
-					local arquivo_sem_extensao=$(ls $(find ~/Downloads -name $dentro_do_arquivo) | awk -F. '{ print $1 }')
-					convert $arquivo_sem_extensao.jpg $arquivo_sem_extensao.png
-				fi
-			done			
-			cd ~/Downloads/imagens-livros
-		else
-			local imagem=$(find ~/Downloads -name $arquivo)
-			local arquivo_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
-			convert $arquivo_sem_extensao.jpg $arquivo_sem_extensao.png
-		fi
-	done
+	local arquivo_sem_extensao=$(ls $1 | awk -F. '{ print $1 }')
+	convert $arquivo_sem_extensao.jpg $arquivo_sem_extensao.png
+
 }
 
-main
+conteudo_subdiretorio(){
+	cd $1
+	for conteudo_subdiretorio in *
+	do
+		if [ -d $(find ~/Downloads/imagens-novos-livros -name $conteudo_subdiretorio) ]
+		then
+			conteudo_subdiretorio $(find ~/Downloads/imagens-novos-livros -name $conteudo_subdiretorio)
+		else
+			converte_imagem  $(find ~/Downloads/imagens-novos-livros -name $conteudo_subdiretorio)
+		fi
+	done
+
+
+}
+
+cd ~/Downloads/imagens-novos-livros
+for arquivo in *
+do
+	if [ -d $arquivo ]
+	then
+		cd $arquivo
+		for dentro_do_arquivo in *
+		do
+			if [ -d $(find ~/Downloads/imagens-novos-livros -name $dentro_do_arquivo) ]
+			then
+				conteudo_subdiretorio $(find ~/Downloads/imagens-novos-livros -name $dentro_do_arquivo)
+			else
+				converte_imagem $(find ~/Downloads/imagens-novos-livros -name $dentro_do_arquivo)
+			fi
+		done
+		cd ~/Downloads/imagens-novos-livros
+	else
+		converte_imagem $(find ~/Downloads/imagens-novos-livros -name $arquivo)
+	fi
+done
