17ce7af69920b47e5ff6838d0084e7cafd7a1e93
RafaelNercessian <rafanercessian@gmail.com>
2017-07-10 10:01:49 -0300

    Update cap1-2-processos-consomem-mais-memoria.sh

--- cap1-2-processos-consomem-mais-memoria.sh
+++ cap1-2-processos-consomem-mais-memoria.sh
@@ -12,8 +12,6 @@ processos_consomem_mais_memoria(){
 		echo -n "$(date +%Y%m%d,%H:%M:%S,)" >> $nome_do_processo.log
 		tamanho=$(ps -o size --pid $pid | grep '[0-9]')
 		echo "$(bc<<<"scale=2;$tamanho/1024") MB " >> $nome_do_processo.log
-		cp $nome_do_processo.log ~/LOG/$nome_do_processo.log
-		rm *.log
 	done
 }
 
